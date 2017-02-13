23aa66bd7e093b080f454fe41916ce7a6fe99360
Chris Kuehl <ckuehl@yelp.com>
2017-02-12 21:56:18 -0800

    Add package boilerplate

---
+++ Makefile
@@ -0,0 +1,25 @@
+REBUILD_FLAG =
+
+.PHONY: minimal
+minimal: venv
+
+.PHONY: venv
+venv: .venv.touch
+	tox -e venv $(REBUILD_FLAG)
+
+.PHONY: test
+test: .venv.tox.touch
+	tox $(REBUILD_FLAG)
+
+.venv.touch .venv.tox.touch: setup.py requirements-dev.txt
+	$(eval REBUILD_FLAG := --recreate)
+	touch "$@"
+
+.PHONY: clean
+clean:
+	find -name '*.pyc' -delete
+	find -name '__pycache__' -delete
+	rm -rf .tox
+	rm -rf venv
+	rm -rf .venv.touch
+	rm -rf .venv.tox.touch
---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='identify',
+    version='0.0.0.dev1',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    install_requires=[],
+    packages=find_packages(exclude=('tests*', 'testing*')),
+)
