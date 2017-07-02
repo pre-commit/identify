2e5201ea68c35be0ae9cdcb899e9090eb3826341
Anthony Sottile <asottile@umich.edu>
2017-07-02 13:14:06 -0700

    v1.0.0

--- setup.py
+++ setup.py
@@ -8,14 +8,24 @@ from setuptools import setup
 
 setup(
     name='identify',
-    version='0.0.4',
+    description='File identification library for Python',
+    url='https://github.com/chriskuehl/identify',
+    version='1.0.0',
+
+    author='Chris Kuehl',
+    author_email='ckuehl@ocf.berkeley.edu',
+
     classifiers=[
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
-    install_requires=[],
+
     packages=find_packages(exclude=('tests*', 'testing*')),
     entry_points={'console_scripts': ['identify-cli=identify.cli:main']},
 )
