bfe8319321a0974aef3c1ebf7ba24af35138c369
Anthony Sottile <asottile@umich.edu>
2017-07-02 20:44:26 -0700

    Add eyaml extension

--- identify/extensions.py
+++ identify/extensions.py
@@ -27,6 +27,7 @@ EXTENSIONS = {
     'eps': {'binary', 'eps'},
     'erb': {'text', 'erb'},
     'exe': {'binary'},
+    'eyaml': {'text': 'yaml'},
     'feature': {'text', 'gherkin'},
     'fish': {'text', 'fish'},
     'gemspec': {'text', 'ruby'},
