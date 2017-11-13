3d65762461f98320aca5646aca49b3885e60264e
Sjoerd Oostdijck <sjoerd@t-l.org>
2017-11-13 15:44:33 +0100

    add file extension for saltstack state files

--- identify/extensions.py
+++ identify/extensions.py
@@ -91,6 +91,7 @@ EXTENSIONS = {
     's': {'text', 'asm'},
     'scss': {'text', 'scss'},
     'sh': {'text', 'shell'},
+    'sls': {'text', 'yaml'},
     'so': {'binary'},
     'spec': {'text', 'spec'},
     'sql': {'text', 'sql'},
