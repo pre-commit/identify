3cd70ac984e63040110c3a5c317c7752f82cf645
Chris Kuehl <ckuehl@yelp.com>
2018-05-24 15:40:47 -0700

    v1.0.18

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name='identify',
     description='File identification library for Python',
     url='https://github.com/chriskuehl/identify',
-    version='1.0.17',
+    version='1.0.18',
 
     author='Chris Kuehl',
     author_email='ckuehl@ocf.berkeley.edu',
