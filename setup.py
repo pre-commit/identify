1af88acc98ca80bf0c716241e35544d18337540f
Anthony Sottile <asottile@umich.edu>
2019-01-01 17:21:01 -0800

    v1.1.8

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name='identify',
     description='File identification library for Python',
     url='https://github.com/chriskuehl/identify',
-    version='1.1.7',
+    version='1.1.8',
 
     author='Chris Kuehl',
     author_email='ckuehl@ocf.berkeley.edu',
