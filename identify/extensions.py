818c07f7562eed43218079b5d469c168d64f93d1
Adolfo Rodriguez <adolfo.rodriguez@shopify.com>
2024-04-25 09:45:44 -0700

    Add ejson extension

--- identify/extensions.py
+++ identify/extensions.py
@@ -56,6 +56,7 @@ EXTENSIONS = {
     'ear': {'binary', 'zip', 'jar'},
     'edn': {'text', 'clojure', 'edn'},
     'ejs': {'text', 'ejs'},
+    'ejson': {'text', 'json', 'ejson'},
     'env': {'text', 'dotenv'},
     'eot': {'binary', 'eot'},
     'eps': {'binary', 'eps'},
