30d1f89096428a1096e8f8169c4d6d6bc8e000a7
Tamás PEREGI <petamas@gmail.com>
2026-02-10 18:04:00 +0100

    Add support for *.entitlements

--- identify/extensions.py
+++ identify/extensions.py
@@ -324,6 +324,7 @@ EXTENSIONS = {
     'zsh': {'text', 'shell', 'zsh'},
 }
 EXTENSIONS_NEED_BINARY_CHECK = {
+    'entitlements': {'plist', 'entitlements'},
     'plist': {'plist'},
     'ppm': {'image', 'ppm'},
 }
